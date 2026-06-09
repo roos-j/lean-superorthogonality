8bc044a542af60ba69ad69acf4ee70bfb73325e1
Joris Roos <roos.jor@gmail.com>
2026-06-09 18:16:29 +0200

    initial commit

---
+++ LeanSuperorthogonality.lean
@@ -0,0 +1,1 @@
+import LeanSuperorthogonality.Basic
---
+++ LeanSuperorthogonality/Basic.lean
@@ -0,0 +1,23 @@
+import Mathlib
+
+noncomputable section
+
+open MeasureTheory
+
+variable {α : Type*} [MeasurableSpace α]
+variable {μ : Measure α}
+
+variable {ι : Type*} [Countable ι]
+
+variable {r : ℕ}
+
+def all_distinct (j : Fin (2 * r) → ι) := ∀ i i', i ≠ i' → j i ≠ j i'
+
+def type_iv_superorthogonal (f : ι → α → ℂ) (r : ℕ) :=
+    ∀ j : Fin (2 * r) → ι, all_distinct j → ∫ x, ∏ i, f (j i) x ∂μ = 0
+
+def sqfct (f : ι → α → ℂ) (x : α) := (∑' j, ‖f j x‖ ^ 2) ^ (2 : ℝ)⁻¹
+
+
+
+end
